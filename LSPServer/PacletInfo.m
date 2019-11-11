bc1607b24661a5f70dee20e086f673d9444c33f0
Brenton Bostick <brenton@wolfram.com>
2019-11-11 10:21:28 -0600

    Add Creator field

--- LSPServer/PacletInfo.m
+++ LSPServer/PacletInfo.m
@@ -3,6 +3,7 @@ Paclet[
     Name -> "LSPServer",
     Version -> "0.15",
     WolframVersion -> "11.0+",
+    Creator ->"Brenton Bostick <brenton@wolfram.com>",
     Extensions -> {
             {"Kernel", Root -> "Kernel", Context -> "LSPServer`"},
             {"Documentation", Language -> "English"}
